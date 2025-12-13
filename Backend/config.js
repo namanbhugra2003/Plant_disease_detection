f388ac398dadbfe281afb18c636fac14608b2a11
namanbhugra2003 <namanbhugra7@gmail.com>
2025-12-13 19:51:15 +0530

    Fourth Commit

---
+++ Backend/config.js
@@ -0,0 +1,6 @@
+export const PORT = 5557;
+
+export const mongoDBURL =
+  "mongodb+srv://mihiran:<dbpassword>@smart-agriguard-mern-fa.zfsfc.mongodb.net/?retryWrites=true&w=majority&appName=smart-agriGuard-Mern-Farmer";
+export const JWT_SECRET = "your_jwt_secret_here";
+export const JWT_EXPIRE = "1h";
