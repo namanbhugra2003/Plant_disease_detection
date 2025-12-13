66cee2faa14f73820e3b5780079367b5274d6e16
Naman Bhugra <namanbhugra7@gmail.com>
2025-12-13 20:17:14 +0530

    Update MongoDB connection string in config.js

--- Backend/config.js
+++ Backend/config.js
@@ -1,6 +1,6 @@
 export const PORT = 5557;
 
 export const mongoDBURL =
-  "mongodb+srv://mihiran:<dbpassword>@smart-agriguard-mern-fa.zfsfc.mongodb.net/?retryWrites=true&w=majority&appName=smart-agriGuard-Mern-Farmer";
+  "mongodb+srv://namanbhugra7_db_user:RSqn6S3MRX5l0igM@cluster1.13nwqo4.mongodb.net/";
 export const JWT_SECRET = "your_jwt_secret_here";
 export const JWT_EXPIRE = "1h";
