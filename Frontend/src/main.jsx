d72b09398d9f728771faf81bd170141726f80aeb
Naman Bhugra <namanbhugra7@gmail.com>
2025-12-16 21:22:56 +0530

    Update main.jsx

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -5,6 +5,7 @@ import { BrowserRouter } from 'react-router-dom'
 import { AnimatePresence } from 'framer-motion'
 import App from './App.jsx'
 import './index.css'
+import "./axiosInterceptor";
 
 try {
   const userString = localStorage.getItem("user");
