4ee2e1581ff0aa2d911ae07369e8a03ff2ca8ff2
Naman Bhugra <namanbhugra7@gmail.com>
2025-12-16 19:54:07 +0530

    Update main.jsx

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,11 +1,19 @@
 
-
+import axios from 'axios'
 import { StrictMode } from 'react' 
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom' 
 import { AnimatePresence } from 'framer-motion'
 import App from './App.jsx'
 import './index.css'
+
+
+const user = JSON.parse(localStorage.getItem("user"));
+
+if (user?.token) {
+  axios.defaults.headers.common["Authorization"] =
+    `Bearer ${user.token}`;
+}
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
